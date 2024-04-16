59f8065689a1122638f4c4fb52a6c193e09c0be2
yashraj thakur <yashrajthakur009@gmail.com>
2024-04-16 18:21:18 +0530

    cors policy changes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path"); // Import path module
 
-const port = 9000;
+const port = process.env.PORT || 9000; // Use port from environment variables or default to 9000
 
 const userRoutes = require("./routes/userRoutes");
 const featureRoutes = require("./routes/FeaturesRoutes.js");
@@ -16,7 +16,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // CORS policy setup to allow requests from any origin
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
+  })
+);
 
 // Define the directory for serving static files (such as images)
 app.use(express.static(path.join(__dirname, "public")));
